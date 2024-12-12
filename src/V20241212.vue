a647e68c2c7af602e68e35f55d5c57990a3ddfa8
yuns84su <yuns84su@naver.com>
2024-12-13 00:10:01 +0900

    vue 연습용

---
+++ src/V20241212.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>안녕!!!!!!!</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld123',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+//import App from './App.vue'
+import App from './V20241212.vue'
+
+createApp(App).mount('#app')
